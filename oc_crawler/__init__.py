82b406a965f324a4eeafe1579bc252b57652f3ea
oceanection <oceanection@gmail.com>
2023-08-12 05:45:11 +0900

    main

--- oc_crawler/__init__.py
+++ oc_crawler/__init__.py
@@ -1,5 +1,7 @@
-from bs4 import BeautifulSoup 
 import requests
+from bs4 import BeautifulSoup
+import numpy as np
+
 from PIL import Image
 import face_recognition
 from chardet import detect
@@ -8,140 +10,172 @@ from io import BytesIO
 import os
 import re
 from urllib.parse import urlparse
+import time
 
-def _base_url(url):
-    r = urlparse(url)
-    return f'{r[0]}://{r[1]}'
 
-class Crawler:
-    def __init__(self, url):
-        self.url = url
-        self.base_url = _base_url(url)
-        self.internal_urls = []
-        self.headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0",
+
+
+
+session = requests.Session()
+
+scraped_urls = []
+downloaded_urls = []
+jpeg_urls = []
+internal_urls = []
+external_urls = []
+
+HEADERS = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0",
                         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
                         "Accept-Language": "ja,en-US;q=0.7,en;q=0.3",
                         "Accept-Encoding": "gzip, deflate, br"}
-        self.session = requests.Session()
-            
-    def get_bs(self):
-        try:
-            session = self.session
-            req = session.get(self.url, headers=self.headers, allow_redirects=True)
-            enc = detect(req.content)
-            if enc['encoding'] is None:
-                return BeautifulSoup(req.content, 'html.parser', from_encoding='utf-8')
-            else:
-                return BeautifulSoup(req.content, 'html.parser', from_encoding=enc['encoding'])
-        except requests.exceptions.RequestException:
-            return None
-        
-    def get_links(self):
-        """ページ内のURLを返す
-        Args:
-            No args.
-        Returns:
-            Pages(List['str',]) | []
-        """
-        bs = self.get_bs()
-        if bs is None:
-            print("ERROR: bs object is None.")
-            return None
-        
-        pages = []
-        for link in bs.find_all('a', href=re.compile(f'^(http|https).*$')):
-            if link.attrs['href'] is not None:
-                pages.append(link.attrs['href'])
-        return list(set(pages))
+
+def add_scraped_urls(url):
+    if url not in scraped_urls:
+        scraped_urls.append(url)
     
-    def get_img(self):
-        """ページ内に<img>タグの"src"属性の値を返す
-        Args:
-            No args.
-        Returns:
-            srcs(List['str',])
-        """
-        bs = self.get_bs()
-        if bs is None:
-            print("ERROR: bs object is None.")
-            return None
-        srcs = []
-        for img in bs.find_all('img', src=re.compile(f'^(http|https).*$')):
-            if img.attrs['src'] is not None:
-                srcs.append(img.attrs['src'])
-        return srcs
+def add_downloaded_urls(url):
+    if url not in downloaded_urls:
+        downloaded_urls.append(url)
 
-    def get_internal_urls(self, urls):
-        """
-        Args:
-            urls (List['str',]):
-        Returns:
-            internal_urls (List['str',]):
-        """
-        internal_urls = [l for l in urls if l.find(self.base_url) != -1]
-        return internal_urls
+def add_jpeg_urls(url):
+    if url not in jpeg_urls:
+        jpeg_urls.append(url)
 
-    def crawl(self):
-        while True:
-            pass
+def add_internal_urls(url):
+    if url not in internal_urls:
+        internal_urls.append(url)
 
-def _is_person(data):
-    """人の顔かどうかを判定する
+def add_external_urls(url):
+    if url not in external_urls:
+        external_urls.append(url)
+
+def summary():
+    print(f'Internal URLs: {len(internal_urls)}')
+    print(f'External URLs: {len(external_urls)}')
+    print(f'Jpeg URLs: {len(jpeg_urls)}')
+    print(f'Downloaded URLs {len(downloaded_urls)}')
+    print(f'Scraped URLs: {len(scraped_urls)}')
+
+def dump(dump_path):
+    d = np.array(internal_urls).reshape((-1, 1))
+    np.savetxt(os.path.join(dump_path, 'internal_urls.csv'), d)
+
+
+
+
+def get_bs(url):
+    add_scraped_urls(url)
+    print(f'Scraping... {url}')
+        
+    try:
+        req = session.get(url, headers=HEADERS, allow_redirects=True)
+        enc = detect(req.content)
+        if enc['encoding'] is None:
+            return BeautifulSoup(req.content, 'html.parser')
+        else:
+            return BeautifulSoup(req.content, 'html.parser', from_encoding=enc['encoding'])
+    except requests.exceptions.RequestException:
+        return None
+
+def is_internal_url(url, base_url):
+    """内部URLかどうかの判定
     Args:
-        data:
+        urls (List['str',]):
     Returns:
-        bool:
+        internal_urls (List['str',]):
     """
-    image = face_recognition.load_image_file(data)
-    face_locations = face_recognition.face_locations(image)
-     
-    if len(face_locations) != 0:
+    if url.find(base_url) != -1:
         return True
     else:
         return False
 
+def is_jpg(url):
+    if url.find('.jpg') != -1:
+        return True
+    else:
+        return False
 
-def _save(img, url, path, x, y, min_x, min_y):
-    if (x >= min_x and y >= min_y):
-        filename = url.split('/')[-1]
-        img.save(os.path.join(path, filename))
-        print('Download: {}'.format(filename))
-     
-class Downloader:
-    def __init__(self, urls, save_path):
-        self.urls = [l for l in urls if l.find('.jpg') != -1]
-        self.save_path = save_path
-        self.headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0",
-                        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-                        "Accept-Language": "ja,en-US;q=0.7,en;q=0.3",
-                        "Accept-Encoding": "gzip, deflate, br"}
-        self.session = requests.Session()
+def get_urls(bs, base_url):
+    """ページ内のhref属性のURLを返す
+    Args:
+        No args.
+    Returns:
+        Pages(List['str',]) | []
+    """
+    if bs is None:
+        print("ERROR: bs object is None.")
+        return None
     
-    def download(self, min_x=25, min_y=25, person=True):
-        """指定のフォルダにダウンロードする。
-        Args:
-            min_x (int):最小の画像の高さの指定
-            min_y (int):最小の画像の幅の指定
-            person (bool):顔認識で人が写っている場合にダウンロードする.初期値はTrue
-        Returns:
+    for link in bs.find_all('a', href=re.compile(f'^(http|https).*$')):
+        if link.attrs['href'] is not None:
+            url = link.attrs['href']            
+            if is_internal_url(url, base_url) and url not in scraped_urls:
+                add_internal_urls(url)
+            else:
+                add_external_urls(url)
+    summary()
+
+
+def download(url, save_path, min_width, min_height):
+    """指定のフォルダにダウンロードする。
+    Args:
+        url (str): ダウンロードURL
+        save_path (str): 保存先パス
+        min_width (int):
+        min_heigght (int):
+    Returns:
+        1 | -1
+    """
+    if url not in downloaded_urls:
+        try:
+            add_downloaded_urls(url)
+            
+            req = session.get(url, headers=HEADERS)
+            if req.content is not None:
+                img = Image.open(BytesIO(req.content))
+                x, y = img.size
+                if (x >= min_width and y >= min_height):
+                    filename = url.split('/')[-1]
+                    img.save(os.path.join(save_path, filename))
+                    print(' Download: {}'.format(filename))
+                    return 1
+            else:
+                return -1
+        except Exception as e:
+            print(f' Download Error: {e}')
+            print(f'Url: {url}')
+            return -1
+
+def get_img(bs, save_path, min_width, min_height):
+    """ページ内に<img>タグの"src"属性の値を返す.
+    jpgファイルがあるときはダウンロードする.
+    
+    Args:
+        bs (BeautifuleSoup):
+    Returns:
+        srcs(List['str',])
+    """
+    if bs is not None:
+        for img in bs.find_all('img', src=re.compile(f'^(http|https).*\.jpg$')):
+            img_url = img.attrs['src']
+            if  img_url is not None:
+                download(img_url, save_path, min_width, min_height)
+    else:
+        return
+
+def crawl(url, save_path, min_width=25, min_height=25):
+    r = urlparse(url)
+    base_url = f'{r[0]}://{r[1]}'
+    
+    # 最初のページをスクレイピング
+    bs = get_bs(url)
+    get_urls(bs, base_url)
+
+    while len(internal_urls) != 0:
+        time.sleep(1)
         
-        """
-        for url in self.urls:
-            try:
-                req = self.session.get(url, headers=self.headers)
-                if req.content is None:
-                    continue
-                elif person:
-                    r = _is_person(BytesIO(req.content))
-                    if r:
-                        img = Image.open(BytesIO(req.content))
-                        x, y = img.size
-                        _save(img, url, self.save_path, x, y, min_x, min_y)
-                    else:
-                        continue
-                else:
-                    _save(img, url, self.save_path, x, y, min_x, min_y)            
-                            
-            except requests.exceptions.RequestException:
-                continue
-        
+        url = internal_urls.pop(0)
+        bs = get_bs(url)
+        if bs is not None:
+            get_img(bs, save_path, min_width, min_height)
+            get_urls(bs, base_url)
