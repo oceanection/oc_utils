12153c30cd4ac5a20196defaf6c9257ead688172
oceanection <oceanection@gmail.com>
2023-08-11 13:10:29 +0900

    main

--- rename.py
+++ rename.py
@@ -1,13 +1,21 @@
 import os
 
 def rename(dir_path):
-    # フォルダ内のファイル名変更
-    
+    """フォルダ内のjpegファイル名を変更する。1から連番で変更する。
+    Args:
+        dir_path (str) :
+    Returns:
+        None
+    """
     rn = 1
     for file in os.listdir(dir_path):
-        os.rename(os.path.join(dir_path,file), os.path.join(dir_path, str(rn)+'.jpg'))
-        rn += 1
+        if (file.find(('.jpg')) != -1):
+            os.rename(os.path.join(dir_path,file), os.path.join(dir_path, str(rn)+'.jpg'))
+            rn += 1
+        else:
+            os.rename(os.path.join(dir_path,file), os.path.join(dir_path, file))
+            
         
 if __name__ == '__main__':
-    dir_path = os.path.join(os.getenv("HOME"),'Pictures', 'sample')
+    dir_path = os.path.join(os.getenv("HOME"), 'Pictures', 'sample')
     rename()
