3b60105b435b2811d76cc4df393a9199f994a757
oceanection <oceanection@gmail.com>
2023-08-25 04:27:17 +0900

    main

--- oc_util/__init__.py
+++ oc_util/__init__.py
@@ -209,8 +209,26 @@ def is_bmp(b: bytes) -> bool:
     """バイナリの先頭部分からWindows Bitmapファイルかどうかを判定する。"""
     return bool(re.match(b"^\x42\x4d", b[:2]))
 
+def put_text(img, up, down, right, left):
+    collor = (0, 255, 140)
+    if up is not None:
+        up = os.path.basename(up)
+        cv2.putText(img, f'Up:{up}', (10, 60),cv2.FONT_HERSHEY_PLAIN, 4, collor, 4, cv2.LINE_AA)
+    
+    if down is not None:
+        down = os.path.basename(down)
+        cv2.putText(img, f'Right:{right}', (10, 130),cv2.FONT_HERSHEY_PLAIN, 4, collor, 4, cv2.LINE_AA)
+    
+    if right is not None:
+        right = os.path.basename(right)
+        cv2.putText(img, f'Down:{down}', (10, 190),cv2.FONT_HERSHEY_PLAIN, 4, collor, 4, cv2.LINE_AA)
+    
+    if left is not None:
+        left = os.path.basename(left)
+        cv2.putText(img, f'Down:{left}', (10, 250),cv2.FONT_HERSHEY_PLAIN, 4, collor, 4, cv2.LINE_AA)
 
-def img_console(dir_path:str, up_key:str, down_key:str, left_key:str, right_key:str):
+
+def img_console(dir_path:str, up_key=None, down_key=None, left_key=None, right_key=None):
     """フォルダ内の画像を１枚ずつ表示して、不要な画像を削除。
     ↑↓→←のキーで指定のフォルダに画像をコピーする。
     Enterで次の画像、Bキーで戻る。
@@ -221,10 +239,12 @@ def img_console(dir_path:str, up_key:str, down_key:str, left_key:str, right_key:
         dir_path (str):
         up_key (str):
         down_key (str):
+        left_key (str):
+        right_key (str):
         copy (bool):
     Returns:
     """
-    if not os.path.exists(dir_path) and not os.os.path.exists(up_key) and not os.os.path.exists(down_key) and not os.os.path.exists(right_key) and not os.os.path.exists(left_key):
+    if not os.path.exists(dir_path):
         return
 
     filelist  = deque([d for d in glob(f'{dir_path}/*') if os.path.isfile(d) and re.search('\.(jpg|png)$', d)])
@@ -235,6 +255,7 @@ def img_console(dir_path:str, up_key:str, down_key:str, left_key:str, right_key:
         print('now: ', filepath)
         
         img = cv2.imread(filepath)
+        put_text(img, up_key, down_key, right_key, left_key)
         cv2.imshow('image', img)
         
         key = cv2.waitKey(0)
@@ -260,23 +281,27 @@ def img_console(dir_path:str, up_key:str, down_key:str, left_key:str, right_key:
             continue
         elif key == 0:
             # Up key
-            shutil.copy2(filepath, up_key)
-            done_filelist.append(filepath) 
+            if up_key is not None:
+                shutil.copy2(filepath, up_key)
+                done_filelist.append(filepath) 
             continue
         elif key == 1:
             # Down Key
-            shutil.copy2(filepath, down_key)
-            done_filelist.append(filepath)
+            if down_key is not None:
+                shutil.copy2(filepath, down_key)
+                done_filelist.append(filepath)
             continue
         elif key == 2:
             # Left Key
-            shutil.copy2(filepath, left_key)
-            done_filelist.append(filepath)
+            if left_key is not None:
+                shutil.copy2(filepath, left_key)
+                done_filelist.append(filepath)
             continue
         elif key == 3:
             # Right key
-            shutil.copy2(filepath, right_key)
-            done_filelist.append(filepath)
+            if right_key is not None:
+                shutil.copy2(filepath, right_key)
+                done_filelist.append(filepath)
             continue
         else:
             # Other
